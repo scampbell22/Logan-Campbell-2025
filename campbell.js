e808a14dbd6768fb00c3ffa4a1f48476aaa1c506
Campbell <Steve.Campbell@Donaldson.com>
2023-09-29 21:35:51 -0500

    reduced margins between header and image

--- campbell.js
+++ campbell.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const card = document.querySelector(".card");
 
-    setTimeout(() => {
-        card.classList.add("flipped");
-    }, 5000);
+    function toggleCardFlip() {
+        card.classList.toggle("flipped");
+    }
+
+    // Handle click event for desktop
+    card.addEventListener("click", toggleCardFlip);
+
+    // Handle touch event for mobile devices
+    card.addEventListener("touchend", toggleCardFlip);
 });
