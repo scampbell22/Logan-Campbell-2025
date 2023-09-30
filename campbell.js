d794a75f3c4f8fe6951b241f3f68af123b324ecc
Campbell <Steve.Campbell@Donaldson.com>
2023-09-29 22:27:15 -0500

    changed timer

--- campbell.js
+++ campbell.js
@@ -1,13 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const card = document.querySelector(".card");
+    let canFlipByTouch = false;  // This flag will determine if the card is allowed to be flipped by touch
 
-    function toggleCardFlip() {
-        card.classList.toggle("flipped");
-    }
+    // Flip the card after 5 seconds
+    setTimeout(() => {
+        card.classList.add("flipped");
 
-    // Handle click event for desktop
-    card.addEventListener("click", toggleCardFlip);
+        // Flip the card back after 60 seconds
+        setTimeout(() => {
+            card.classList.remove("flipped");
+            canFlipByTouch = true;  // Allow the card to be flipped by touch after the sequence
+        }, 60000); // 60 seconds
 
-    // Handle touch event for mobile devices
-    card.addEventListener("touchend", toggleCardFlip);
+    }, 5000); // 5 seconds
+
+    card.addEventListener("click", function() {
+        if (canFlipByTouch) {
+            if (card.classList.contains("flipped")) {
+                card.classList.remove("flipped");
+            } else {
+                card.classList.add("flipped");
+            }
+        }
+    });
 });
